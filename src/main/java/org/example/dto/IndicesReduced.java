5f9d451dd61091233800b757243f07e731d63f8a
bogdan <b.pyanzin@mail.ru>
2024-01-12 04:00:12 +0700

    fix 1

--- src/main/java/org/example/dto/IndicesReduced.java
+++
@@ -1,7 +0,0 @@
-package org.example.dto;
-
-import java.util.List;
-
-public record IndicesReduced(List<Integer> indicesBullets, List<Integer> indicesHelicopters,
-                             List<Integer> indicesParatroopers) {
-}
