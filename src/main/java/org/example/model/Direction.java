35ff5eeb9d399fc8600d58eab3adc56056a58783
bogdan <b.pyanzin@mail.ru>
2024-06-30 20:13:42 +0700

    to git

---
+++ src/main/java/org/example/model/Direction.java
@@ -0,0 +1,17 @@
+package org.example.model;
+public enum Direction {
+    LEFT {
+        @Override
+        int delta(int speed) {
+            return -speed;
+        }
+    },
+    RIGHT {
+        @Override
+        int delta(int speed) {
+            return speed;
+        }
+    };
+
+    abstract int delta(int speed);
+}
